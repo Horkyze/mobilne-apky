bf0606bab2d08da2b31b3d1702f71af9308d78a9
simon741 <ssimon007@gmail.com>
2016-04-10 14:10:26 +0200

    adjust layout of create revizor screen

--- app/src/main/java/sk/stuba/fiit/revizori/CreateRevizorActivity.java
+++ app/src/main/java/sk/stuba/fiit/revizori/CreateRevizorActivity.java
@@ -1,15 +1,26 @@
 package sk.stuba.fiit.revizori;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import sk.stuba.fiit.revizori.model.Revizor;
 import sk.stuba.fiit.revizori.service.RevizorService;
 
-public class CreateRevizorActivity extends AppCompatActivity {
+public class CreateRevizorActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +34,44 @@ public class CreateRevizorActivity extends AppCompatActivity {
             }
         });
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+       /* SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map_p);
+        mapFragment.getMapAsync(this);*/
     }
 
     public void onCreatePostClick(){
-        TextView line = (TextView) findViewById(R.id.add_line_number);
+        TextView line = (TextView) findViewById(R.id.line_number);
         TextView comment = (TextView) findViewById(R.id.comment);
         Revizor r = new Revizor(line.getText().toString(), Math.random(), Math.random(), "photourl", comment.getText().toString());
         RevizorService.getInstance().createRevizor(r);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
 }
--- app/src/main/java/sk/stuba/fiit/revizori/SubmissionDetailActivity.java
+++ app/src/main/java/sk/stuba/fiit/revizori/SubmissionDetailActivity.java
@@ -2,6 +2,7 @@ package sk.stuba.fiit.revizori;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +12,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class SubmissionDetailActivity extends FragmentActivity implements OnMapReadyCallback {
+public class SubmissionDetailActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
