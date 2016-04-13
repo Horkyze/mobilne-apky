a65c92e7c3b596ed1d0632e631efdb191ca6a4a8
simon741 <ssimon007@gmail.com>
2016-04-13 19:45:55 +0200

    merge fix

--- app/src/main/java/sk/stuba/fiit/revizori/MainActivity.java
+++ app/src/main/java/sk/stuba/fiit/revizori/MainActivity.java
@@ -225,10 +225,6 @@ public class MainActivity extends AppCompatActivity implements
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -259,7 +255,6 @@ public class MainActivity extends AppCompatActivity implements
         SyncAdapter.syncImmediately(Revizori.getAppContext());
 
         swipeRefreshLayout.setRefreshing(false);
-
 
     }
 
